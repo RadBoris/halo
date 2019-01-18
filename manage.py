0f14e077cab558ad5557fa197ec703c4f120b7f9
RadBoris <rad.borislavov@gmail.com>
2019-01-18 13:06:45 +0000

    more fixes

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
+from manage import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ubzdjahonslgwk:4516f846862bbb569079f2b53bcccd8a145315c17098df6390746eee91fd9995@ec2-184-72-230-93.compute-1.amazonaws.com:5432/daco986o48khus'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
