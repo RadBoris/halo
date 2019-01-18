230e77fd944c517d853a9ba2d52e6b38582d84b0
RadBoris <rad.borislavov@gmail.com>
2019-01-18 13:02:54 +0000

    add proc and migrate

---
+++ Procfile
@@ -0,0 +1,2 @@
+web python3 routes.py  -i 0.0.0.0 -p $PORT 
+release: python manage.py db upgrade
---
+++ manage.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class User(Base):
+  __tablename__ = 'users'
+  uid = Column(Integer, primary_key = True)
+  firstname = Column(String(100))
+  lastname = Column(String(100))
+  email = Column(String(120), unique=True)
+  pwdhash = Column(String(54))
+
+  def __init__(self, firstname, lastname, email, password):
+    self.firstname = firstname.title()
+    self.lastname = lastname.title()
+    self.email = email.lower()
+    self.set_password(password)
+
+  def set_password(self, password):
+    self.pwdhash = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.pwdhash, password)
+
+class Info(Base):
+  __tablename__ = 'info'
+  id = Column(Integer, primary_key = True)
+  key = Column(String(50))
+  value = Column(String(50))
+  user_id = Column(Integer, ForeignKey('users.uid'))
+
+  def __init__(self, key, value, user_id):
+    self.key = key
+    self.value = value
+    self.user_id = user_id
+
+if __name__ == '__main__':
+    manager.run()
