d26d749db1557b1f90ada6e4b36ee8b2595162bb
RadBoris <rad.borislavov@gmail.com>
2019-01-18 06:59:42 +0000

    initial

---
+++ models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from werkzeug import generate_password_hash, check_password_hash
+from sqlalchemy import Column, Integer, String
+from database import Base
+from sqlalchemy.ext.declarative import ConcreteBase
+from sqlalchemy import and_, or_, not_
+
+class User(Base):
+  __tablename__ = 'users'
+  uid = Column(Integer, primary_key = True)
+  firstname = Column(String(100))
+  lastname = Column(String(100))
+  email = Column(String(120), unique=True)
+  pwdhash = Column(String(54))
+
+  def __init__(self, firstname, lastname, email, password):
+    self.firstname = firstname.title()
+    self.lastname = lastname.title()
+    self.email = email.lower()
+    self.set_password(password)
+     
+  def set_password(self, password):
+    self.pwdhash = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.pwdhash, password)
+
+class Info(Base):
+  __tablename__ = 'info'
+  id = Column(Integer, primary_key = True) 
+  key = Column(String(50))
+  value = Column(String(50))
+  user_id = Column(Integer, ForeignKey('users.uid'))
+  
+  def __init__(self, key, value, user_id):
+    self.key = key
+    self.value = value
+    self.user_id = user_id 
+   
---
+++ routes.py
@@ -0,0 +1,139 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import User, Info
+from forms import SignupForm, LoginForm, InfoForm, SearchForm
+from database import db_session
+from sqlalchemy import and_, or_, not_
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] =" postgres://ubzdjahonslgwk:4516f846862bbb569079f2b53bcccd8a145315c17098df6390746eee91fd9995@ec2-184-72-230-93.compute-1.amazonaws.com:5432/daco986o48khus"
+
+
+app.secret_key = "development-key"
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+  if 'email' not in session:
+    return redirect(url_for('login'))
+
+  form = SearchForm()
+  cid = session['id']
+  email = session['email']
+  user = User.query.filter_by(email=email).first()
+  keys = Info.query
+  results = []
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      if form.search.data is not None:
+        results = db_session.query(Info).filter(Info.key.like('%' + form.search.data + '%'), Info.user_id==cid)
+        #results = db_session.query(Info).filter_by(user_id=id).all()
+     #qry = db_session.query(Info).filter_by(search=key).all()
+        #results = keys.all()
+        return render_template('results.html', results=results)
+     # db_session.add(newuser)
+     # db_session.commit()
+     # session['id'] = newuser.uid
+     # session['email'] = newuser.email
+     # return redirect(url_for('about'))
+
+  elif request.method == "GET":
+    return render_template('about.html', form=form)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+  if 'email' in session:
+    return redirect(url_for('home'))
+
+  form = SignupForm()
+
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db_session.add(newuser)
+      db_session.commit()
+      session['id'] = newuser.uid
+      session['email'] = newuser.email
+      return redirect(url_for('home'))
+
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if 'email' in session:
+    return redirect(url_for('home'))
+
+  form = LoginForm()
+
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template("login.html", form=form)
+    else:
+      email = form.email.data 
+      password = form.password.data 
+
+      user = db_session.query(User).filter(User.email==email).first()
+      
+      if user is not None and user.check_password(password):
+        session['email'] = user.email
+        session['id'] = user.uid 
+        return redirect(url_for('home'))
+      else:
+        return redirect(url_for('login'))
+
+  elif request.method == 'GET':
+    return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+  session.pop('email', None)
+  return redirect(url_for('index'))
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+  if 'email' not in session:
+    return redirect(url_for('login'))
+  email = session['email']
+  form = InfoForm()
+  key = ''
+  value= ''
+  
+  user = db_session.query(User).filter(User.email==email).first()
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('home.html', form=form)
+    else:
+      
+      
+      
+      key = form.key.data
+      value = form.value.data 
+      newinfo = Info(form.key.data, form.value.data, user.uid)
+      db_session.add(newinfo)
+      db_session.commit()
+
+      # return those results
+      return render_template('home.html', form=form, key=key, value=value)
+
+  elif request.method == 'GET':
+    return render_template("home.html", form=form, key=key, value=value, user=user)
+
+@app.route("/results")
+def results():
+  if 'email' not in session:
+    return redirect(url_for('login'))
+   
+  return render_template('results.html')
+
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
